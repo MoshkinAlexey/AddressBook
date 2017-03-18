08645193b41b5cb66ed1d2437fa40a010cd39515
MoshkinAlexey <lesha_43rus@mail.ru>
2017-03-18 18:12:48 +0300

    Initial commit

---
+++ src/test/java/Tests.java
@@ -0,0 +1,39 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+import java.util.ArrayList;
+
+/**
+ * Тесты для адресной книги
+ */
+
+public class Tests {
+    @Test
+    public void checkBook() {
+        Address address1 = new Address("ул. Политехническая", 19, 29),
+                address2 = new Address("ул. Политехническая", 17, 34),
+                address3 = new Address("пр. Науки", 2, 8),
+                address4 = new Address("пр. Непокоренных", 8, 123);
+
+        String person1 = "Волков",
+                person2 = "Петров",
+                person3 = "Шурыгина",
+                person4 = "Иванова",
+                person5 = "Креницин";
+
+        AddressBook newBook = new AddressBook();
+
+        newBook.add(person1, address1);
+        newBook.add(person2, address2);
+        newBook.add(person1, address3);
+        newBook.add(person3, address3);
+        newBook.add(person4, address4);
+        newBook.add(person5, address2);
+
+        ArrayList list = new ArrayList<String>();
+        list.add(person1);
+        list.add(person5);
+        list.add(person2);
+
+        assertEquals(list, newBook.getList(address1.getStreet()));
+    }
+}
